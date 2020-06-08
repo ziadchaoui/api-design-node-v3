a8650945bb4d537acc1f1dc1f5c6076eca2c4925
Ziad Chaoui <chaoui@eye-square.com>
2020-06-08 14:39:50 +0200

    lesson 3

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,27 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true, maxlength: 50 },
+    status: {
+      default: 'active',
+      enum: ['active', 'complete', 'pastdue'],
+      required: true,
+      type: String
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      ref: 'user',
+      required: true,
+      type: mongoose.SchemaTypes.ObjectId
+    },
+    list: {
+      ref: 'list',
+      required: true,
+      type: mongoose.SchemaTypes.ObjectId
+    }
+  },
+  { timestamps: true }
+)
 export const Item = mongoose.model('item', itemSchema)
